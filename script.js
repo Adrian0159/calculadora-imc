0dddb09e5d95df340f900c56b0eac8ed7363574b
Adriano Gonçalves da Silva <88063170+Adrian0159@users.noreply.github.com>
2024-05-23 20:04:06 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const nome = document.querySelector("#nome");
+const altura = document.querySelector("#altura");
+const peso = document.querySelector("#peso");
+const btn = document.querySelector("#btn");
+const res = document.querySelector("#resultado");
+
+$(document).ready(function () {
+  $("#altura").mask("0.00");
+  $("#peso").mask("000");
+});
+
+btn.addEventListener("click", () => {
+  let alt = Number(altura.value);
+  let pso = Number(peso.value);
+  let imc = pso / (alt * alt);
+  let IMC = imc.toFixed(1);
+
+  if (IMC < 17) {
+    res.innerHTML = `${nome.value} seu  <strong>IMC</strong> é <strong>${IMC}</strong>. você está muito abaixo do peso. Precisa ganhar uns quilinhos!`;
+  } else if (IMC < 18.5) {
+    res.innerHTML = `${nome.value} seu <strong>IMC</strong> é <strong>${IMC}</strong>. você está abaixo do peso.`;
+  } else if (IMC < 25) {
+    res.innerHTML = `${nome.value} seu <strong>IMC</strong> é <strong>${IMC}</strong>. você está com o peso ideal. Parabêns!`;
+  } else if (IMC < 30) {
+    res.innerHTML = `${nome.value} seu <strong>IMC</strong> é <strong>${IMC}</strong>. você está acima do peso.`;
+  } else if (IMC < 35) {
+    res.innerHTML = `${nome.value} seu <strong>IMC</strong> é <strong>${IMC}</strong>. você está com obesidade grau I.`;
+  } else if (IMC < 40) {
+    res.innerHTML = `${nome.value} seu <strong>IMC</strong> é <strong>${IMC}</strong>. você está com obesidade grau II.`;
+  } else {
+    res.innerHTML = `${nome.value} seu <strong>IMC</strong> é <strong>${IMC}</strong>. você está com obesidade grau III. Melhor fazer uma dieta!`;
+  }
+});
